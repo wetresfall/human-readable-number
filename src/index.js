9a192a8b0ab6c82cc991becd897008b79ce296d8
Jack Belnik <jw.belnik@gmail.com>
2023-07-31 17:15:06 +0300

    feat: add arrays

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 module.exports = function toReadable(number) {
+  const ones = [
+                '', 
+                'one', 
+                'two', 
+                'three', 
+                'four', 
+                'five', 
+                'six', 
+                'seven', 
+                'eight', 
+                'nine'
+              ];
+
+  const teens = [
+                 'ten', 
+                 'eleven', 
+                 'twelve', 
+                 'thirteen', 
+                 'fourteen', 
+                 'fifteen', 
+                 'sixteen', 
+                 'seventeen', 
+                 'eighteen', 
+                 'nineteen'
+               ];
+
+  const tens = [
+                '', 
+                '', 
+                'twenty', 
+                'thirty', 
+                'forty', 
+                'fifty', 
+                'sixty', 
+                'seventy', 
+                'eighty', 
+                'ninety'
+              ];
+
   let result = '';
 
-  if (number === 0) {
-    return 'zero';
-  }
 
-  
+
 };
