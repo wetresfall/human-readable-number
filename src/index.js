87eedef54dafe4bf5c68bb175fbc3938eb9440d0
Jack Belnik <jw.belnik@gmail.com>
2023-07-31 17:22:24 +0300

    feat: add three and four rules

--- src/index.js
+++ src/index.js
@@ -46,6 +46,19 @@ module.exports = function toReadable(number) {
       result += ones[Math.floor(number / 100)] + ' hundred ';
       number %= 100;
     }
+
+    if (number >= 10 && number <= 19) {
+      result += teens[number - 10];
+      number = 0;
+    } else if (number >= 20) {
+      result += tens[Math.floor(number / 10)];
+      number %= 10;
+      if (number > 0) result += ' ';
+    }
+
+    if (number > 0) {
+      result += ones[number];
+    }
   
     return result.trim();
 
